555536b48ee9958b190c5919463b7d50651da5b9
HaniefuL <haniefulgeofarry@gmail.com>
2025-10-16 19:17:19 +0700

    mengubah color read menjadi blue

--- app/src/main/java/com/example/tataletak/tataletak.kt
+++ app/src/main/java/com/example/tataletak/tataletak.kt
@@ -142,13 +142,14 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             contentScale = ContentScale.Fit)
         Text(text = "My Music",
             fontSize = 50.sp,
-            color = Color.Red,
+            color = Color.Blue,
             fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.Cursive,
             modifier = Modifier.align(
                 alignment = Alignment.Center))
     }
 }
+
 
 
 
